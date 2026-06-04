a2ef4df8788fd371b8094938dd57f870d93c348d
khoapham <khoapham2509fb@gmail.com>
2026-06-04 22:02:15 +0700

    BE KHOAPHAM

--- backend/src/routes/trip.routes.js
+++ backend/src/routes/trip.routes.js
@@ -4,6 +4,8 @@ const {
   getTrips,
   getTripById,
   updateTrip,
+  getPackingList,
+  updatePackingList,
   deleteTrip,
   shareTrip,
   getSharedTrip,
@@ -26,6 +28,10 @@ router.route('/:id')
   .get(getTripById)
   .put(updateTrip)
   .delete(deleteTrip);
+
+router.route('/:id/packing-list')
+  .get(getPackingList)
+  .put(updatePackingList);
 
 router.post('/:id/share', shareTrip);
 
