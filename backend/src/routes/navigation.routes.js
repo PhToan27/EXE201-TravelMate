8a89e0b6b97793fadb56a19b32e36905df82a809
Vo Chieu Quan <vochieuquan26@gmail.com>
2026-06-15 17:49:39 +0700

    bando

---
+++ backend/src/routes/navigation.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { getNavigationToPlace } = require('../controllers/navigation.controller');
+const { protect } = require('../middlewares/auth.middleware');
+
+const router = express.Router();
+
+router.use(protect);
+router.get('/place/:placeId', getNavigationToPlace);
+
+module.exports = router;
