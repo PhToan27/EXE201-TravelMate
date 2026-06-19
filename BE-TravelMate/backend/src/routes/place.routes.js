aad06a87626c20340b73b5a8f6e4ce40a820a68a
Vo Chieu Quan <vochieuquan26@gmail.com>
2026-06-19 19:50:35 +0700

    Make place search public

--- BE-TravelMate/backend/src/routes/place.routes.js
+++ BE-TravelMate/backend/src/routes/place.routes.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const { getPlaceDetails, searchPlaces } = require('../controllers/place.controller');
-const { protect } = require('../middlewares/auth.middleware');
 
 const router = express.Router();
-
-// Protect all routes under /api/places
-router.use(protect);
 
 router.get('/search', searchPlaces);
 router.get('/detail', getPlaceDetails);
